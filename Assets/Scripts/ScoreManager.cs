74d4ac93ff3a3ddf7885cf890daa05e8291ef8d1
aedgaras <66108869+aedgaras@users.noreply.github.com>
2021-03-29 20:58:29 +0300

    Particle effects
    Added  particle effects for coin change, lights, shadows.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,11 +10,15 @@ public class ScoreManager : MonoBehaviour
     public TextMeshProUGUI text;
 
     public int score = 0;
+
+    public ParticleSystem particleSystem;
+
     // Start is called before the first frame update
     void Start()
     {
         if (instance == null)
         {
+            particleSystem.GetComponent<ParticleSystem>();
             instance = this;
         }
         
@@ -23,6 +27,7 @@ public class ScoreManager : MonoBehaviour
     // Updatinamas tekstas kai paimamas coin
     public void ChangeScore(int coinValue)
     {
+        particleSystem.Play();
         score += coinValue;
         text.text = "x" + score.ToString();
     }
