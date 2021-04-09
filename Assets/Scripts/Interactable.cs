74ae3043ac6d90ffefde288ea13aa7edf0cf48e7
Augustas <jakaitisaugustas@gmail.com>
2021-04-09 19:25:54 +0300

    chest animation

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public abstract class Interactable : MonoBehaviour
+{
+    public bool isColided = false;
+    private void Reset()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    public abstract void Interact();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isColided != true)
+        {
+            if(collision.CompareTag("Player"))
+                collision.GetComponent<PrototypeHeroDemo>().OpenInteractableIcon();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            collision.GetComponent<PrototypeHeroDemo>().CloseInteractableIcon();
+    }
+}
---
+++ Assets/Scripts/Openable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Openable : Interactable
+{
+    public Sprite open;
+    public Sprite closed;
+
+    private SpriteRenderer sr;
+    private bool isClosed = true;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = closed;
+    }
+
+    public override void Interact()
+    {
+        if (isClosed)
+        {
+            sr.sprite = open;
+            isClosed = !isClosed;
+        }
+    }
+}
