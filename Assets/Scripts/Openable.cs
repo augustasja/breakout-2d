c77de473829acf50e859b365920750f92e4294ab
aedgaras <66108869+aedgaras@users.noreply.github.com>
2021-04-09 20:30:14 +0300

    Chest rewards and sounds

--- Assets/Scripts/Openable.cs
+++ Assets/Scripts/Openable.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
@@ -11,6 +9,11 @@ public class Openable : Interactable
 
     private SpriteRenderer sr;
     private bool isClosed = true;
+
+    public AudioClip chestSound;
+    public AudioClip coinSound;
+
+    public int reward;
 
     private void Start()
     {
@@ -24,6 +27,16 @@ public class Openable : Interactable
         {
             sr.sprite = open;
             isClosed = !isClosed;
+
+            StartCoroutine(PlaySoundWithDelay(0f, chestSound));
+            StartCoroutine(PlaySoundWithDelay(1.5f, coinSound));
+            ScoreManager.instance.ChangeScore(reward);
         }
+    }
+
+    private IEnumerator PlaySoundWithDelay(float delay, AudioClip audio)
+    {
+        yield return new WaitForSeconds(delay);
+        AudioSource.PlayClipAtPoint(audio, transform.position);
     }
 }
